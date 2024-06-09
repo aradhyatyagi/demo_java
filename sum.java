f3898d462d2e896394756ed1050455464d73ac55
aradhyatyagi <157880220+aradhyatyagi@users.noreply.github.com>
2024-06-09 21:48:46 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,29 @@
+package Javapackage;
+
+import java.util.Scanner;
+public class sum {
+    
+	public static void main(String[] args)
+	{   
+		Scanner input =new Scanner(System.in);
+		System.out.print("Enter the Array Limit :");
+		int l =input.nextInt();
+		int [] a =new int[l];
+		int sum = 0;
+		for(int i=0;i<l;i++)
+		{
+			System.out.printf("Element of a[%d]:",i);
+			a[i]=input.nextInt();
+		}
+		for(int o:a)
+		{
+			if(o%2==1)
+				sum = sum + o;
+		}		
+		System.out.println("Sum:"+sum);
+    }
+}
+
+
+    
+
